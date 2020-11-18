dc8d44a2f2a575a40e7d06fc81a98ca1fe52854a
Akilesh Rao <akilesh.rao@routemobile.com>
2020-11-18 17:04:24 +0530

    basic logic added

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 import Deck from './deck.js';
+import { Card } from './deck.js';
 
 const CARD_WEIGHTS = {
     "2": 2,
@@ -21,7 +22,10 @@ let playerDeck, computerDeck;
 const playerCardElement = document.querySelector(".player-card");
 const computerCardElement = document.querySelector(".computer-card");
 const playerDeckElement = document.querySelector(".player-deck");
-const computerDeckElement = document.querySelector(".computer-deck")
+const computerDeckElement = document.querySelector(".computer-deck");
+const playerCount = document.querySelector(".player-count");
+const computerCount = document.querySelector(".computer-count");
+
 const deck = new Deck();
 deck.shuffle();
 
@@ -31,25 +35,38 @@ playerCardElement.addEventListener('click', () => flipCards());
 const startGame = () => {
     let deck = new Deck();
     deck.shuffle();
-    playerDeck = new Deck(deck.cards.slice(0, deck.midPoint));
+    playerDeck = new Deck([new Card("♠", "2")]);
     computerDeck = new Deck(deck.cards.slice(deck.midPoint, deck.noOfCards));
+    updateCount();
 }
 
 const flipCards = () => {
     const playerCard = playerDeck.cards.pop();
     const computerCard = computerDeck.cards.pop();
-    if (CARD_WEIGHTS[playerCard.value] > CARD_WEIGHTS[computerCard.value]) {
-        playerDeck.cards.push(playerCard);
-        playerDeck.cards.push(computerCard);
-        console.log(playerDeck, computerDeck);
+    if (playerDeck.noOfCards > 0 || computerDeck.noOfCards > 0) {
+        if (CARD_WEIGHTS[playerCard.value] > CARD_WEIGHTS[computerCard.value]) {
+            playerDeck.cards.unshift(playerCard);
+            playerDeck.cards.unshift(computerCard);
+        } else if (CARD_WEIGHTS[playerCard.value] < CARD_WEIGHTS[computerCard.value]) {
+            computerDeck.cards.unshift(playerCard);
+            computerDeck.cards.unshift(computerCard);
+        } else {
+            computerDeck.cards.unshift(computerCard);
+            playerDeck.cards.unshift(playerCard);
+        }
     } else {
-        computerDeck.cards.push(playerCard);
-        computerDeck.cards.push(computerCard);
-        console.log(playerDeck, computerDeck);
+        console.log("Won");
     }
 
+    playerCount.innerText = playerDeck.noOfCards;
+    computerCount.innerText = computerDeck.noOfCards;
+
+}
+
+const updateCount = () => {
+    playerCount.innerText = playerDeck.noOfCards;
+    computerCount.innerText = computerDeck.noOfCards;
 }
 
 startGame();
 
-console.log(deck);
